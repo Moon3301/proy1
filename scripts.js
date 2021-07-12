0e3926d02461878dd54a17c6984acc73310acfe5
Moon3301 <carlos_db_16@live.cl>
2021-07-12 01:57:50 -0400

    commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+
+// Get the element with id="defaultOpen" and click on it
+
+
